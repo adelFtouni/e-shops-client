4bbed108393928a5bc87231a3ddeea2bf519290e
adelftouni <adel.ftouni020@gmail.com>
2025-10-29 06:46:32 +0300

    ci/cd settings

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/client-e-shops/',  // Adjust this to match your GitHub repo name
   plugins: [
     tailwindcss(),
   ],
